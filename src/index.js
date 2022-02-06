1fa8a7d6452c01792e736cb27787476bddc4a162
Mathias <mathiaslammers@gmail.com>
2022-02-06 17:51:10 +0100

    Added UniqueWords function

--- src/index.js
+++ src/index.js
@@ -14,18 +14,39 @@ app.use(
 
 // Define POST endpoint
 app.post("/count", (req, res) => {
-  var word = "banan"; // For testing purpose
   var text = req.body.text;
-  var nOccurances = WordCount(word, text);
-  res.send(`${word} occurs ${nOccurances} times in text: ${text}`);
+  var words = UniqueWords(text);
+  res.send("ok");
 });
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
 
+/**
+ * Count the case insensitive occurrences of a substring in a string;
+ * @param {String} word   The word to search for
+ * @param {String} text   The text to search in
+ * @returns               Number of occurrences
+ */
 function WordCount(word, text) {
-  var re = new RegExp(`\\b${word}\\b`, "g");
-  var count = (text.match(re) || []).length;
+  var re = new RegExp(`${word}`, "gi");
+  var count = text.match(re).length;
   return count;
+}
+
+/**
+ * Get unique words in a text;
+ * @param {String} text   The text to get words from
+ * @returns               Array of unique words
+ */
+function UniqueWords(text) {
+  var words = text
+    .replace(/[,.?!]/g, "")
+    .split(" ")
+    .map((word) => {
+      return word.toLowerCase();
+    });
+  var uniqueWords = [...new Set(words)];
+  return uniqueWords;
 }
